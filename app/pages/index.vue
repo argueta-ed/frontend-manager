91512348d9db753c0b227ac75a9b074d6aba5fc8
Edwin Argueta MIS <edwin@makeitsimple.io>
2025-08-03 17:20:46 -0600

    Create project index file and add redirect middleware

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  // to control redirection based on authentication status.
+  definePageMeta({
+    middleware: 'redirect-home'
+  })
+
+  useHead({
+    title: 'App Manager | Home'
+  })
+</script>
+
+<style></style>
